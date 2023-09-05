a85856d22f191ac4ae4830017ef5a5d11dcc0b17
Rob Gonnella <robgonnella@gmail.com>
2023-09-05 09:07:31 -0400

    Add LICENSE

--- examples/fullscan.go
+++ examples/fullscan.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // Full (ARP + SYN) Scanning example
 package main
 
--- examples/synscan.go
+++ examples/synscan.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // SYN Scanning example
 package main
 
