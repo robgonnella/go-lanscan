39967d1920c2df9abb092435c7f6d76a7ed6b180
Rob Gonnella <robgonnella@gmail.com>
2023-10-29 13:00:20 -0400

    Adds test coverage for version-control errors

--- internal/scripts/bump-version/version/version_test.go
+++ internal/scripts/bump-version/version/version_test.go
@@ -3,6 +3,7 @@
 package version_test
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 
@@ -64,5 +65,127 @@ func TestBumpVersion(t *testing.T) {
 		err := version.Bump(execData, mockVg, mockVc)
 
 		assert.NoError(st, err)
+	})
+
+	t.Run("returns error if vg.Generate returns error", func(st *testing.T) {
+		mockVc := mock_version.NewMockVersionControl(ctrl)
+		mockVg := mock_version.NewMockVersionGenerator(ctrl)
+
+		versionStr := "v2.2.2"
+		outFile := "outfile.go"
+		templatePath := "outfile.go.tmpl"
+
+		execData := version.BumpData{
+			Version:      versionStr,
+			OutFile:      outFile,
+			TemplatePath: templatePath,
+		}
+
+		data := version.VersionData{
+			VERSION: versionStr,
+		}
+
+		expectedErr := errors.New("mock generate error")
+
+		mockVg.EXPECT().Generate(data).Return(expectedErr)
+
+		err := version.Bump(execData, mockVg, mockVc)
+
+		assert.Error(st, err)
+		assert.ErrorIs(st, expectedErr, err)
+	})
+
+	t.Run("returns error if vc.Add returns error", func(st *testing.T) {
+		mockVc := mock_version.NewMockVersionControl(ctrl)
+		mockVg := mock_version.NewMockVersionGenerator(ctrl)
+
+		versionStr := "v2.2.2"
+		outFile := "outfile.go"
+		templatePath := "outfile.go.tmpl"
+
+		execData := version.BumpData{
+			Version:      versionStr,
+			OutFile:      outFile,
+			TemplatePath: templatePath,
+		}
+
+		data := version.VersionData{
+			VERSION: versionStr,
+		}
+
+		expectedErr := errors.New("mock git add error")
+
+		mockVg.EXPECT().Generate(data)
+		mockVc.EXPECT().Add(outFile).Return(expectedErr)
+
+		err := version.Bump(execData, mockVg, mockVc)
+
+		assert.Error(st, err)
+		assert.ErrorIs(st, expectedErr, err)
+	})
+
+	t.Run("returns error if vc.Commit returns error", func(st *testing.T) {
+		mockVc := mock_version.NewMockVersionControl(ctrl)
+		mockVg := mock_version.NewMockVersionGenerator(ctrl)
+
+		versionStr := "v2.2.2"
+		outFile := "outfile.go"
+		templatePath := "outfile.go.tmpl"
+
+		execData := version.BumpData{
+			Version:      versionStr,
+			OutFile:      outFile,
+			TemplatePath: templatePath,
+		}
+
+		data := version.VersionData{
+			VERSION: versionStr,
+		}
+
+		message := fmt.Sprintf("Bump version %s", execData.Version)
+
+		expectedErr := errors.New("mock git commit error")
+
+		mockVg.EXPECT().Generate(data)
+		mockVc.EXPECT().Add(outFile)
+		mockVc.EXPECT().Commit(message).Return(expectedErr)
+
+		err := version.Bump(execData, mockVg, mockVc)
+
+		assert.Error(st, err)
+		assert.ErrorIs(st, expectedErr, err)
+	})
+
+	t.Run("returns error if vc.Tag returns error", func(st *testing.T) {
+		mockVc := mock_version.NewMockVersionControl(ctrl)
+		mockVg := mock_version.NewMockVersionGenerator(ctrl)
+
+		versionStr := "v2.2.2"
+		outFile := "outfile.go"
+		templatePath := "outfile.go.tmpl"
+
+		execData := version.BumpData{
+			Version:      versionStr,
+			OutFile:      outFile,
+			TemplatePath: templatePath,
+		}
+
+		data := version.VersionData{
+			VERSION: versionStr,
+		}
+
+		message := fmt.Sprintf("Bump version %s", execData.Version)
+
+		expectedErr := errors.New("mock git tag error")
+
+		mockVg.EXPECT().Generate(data)
+		mockVc.EXPECT().Add(outFile)
+		mockVc.EXPECT().Commit(message)
+		mockVc.EXPECT().Tag(versionStr).Return(expectedErr)
+
+		err := version.Bump(execData, mockVg, mockVc)
+
+		assert.Error(st, err)
+		assert.ErrorIs(st, expectedErr, err)
 	})
 }
