bc10c1c265404e3df9c4522347cbd487cd75a3b7
Rob Gonnella <robgonnella@gmail.com>
2023-11-07 02:37:38 -0500

    Increase test coverage on arpScanner

---
+++ pkg/internal/test-helper/test-helper.go
@@ -0,0 +1,72 @@
+package test_helper
+
+import (
+	"net"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+)
+
+func NewArpReplyReadResult(srcIP net.IP, srcHwAddr net.HardwareAddr) (data []byte, ci gopacket.CaptureInfo, err error) {
+	eth := layers.Ethernet{
+		SrcMAC:       srcHwAddr,
+		DstMAC:       []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+		EthernetType: layers.EthernetTypeARP,
+	}
+
+	arp := layers.ARP{
+		AddrType:          layers.LinkTypeEthernet,
+		Protocol:          layers.EthernetTypeIPv4,
+		HwAddressSize:     6,
+		ProtAddressSize:   4,
+		Operation:         layers.ARPReply,                            // ARP reply
+		SourceHwAddress:   srcHwAddr,                                  // Your MAC address
+		SourceProtAddress: []byte(srcIP.To4()),                        // Your IP address
+		DstHwAddress:      []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, // Target MAC address
+		DstProtAddress:    []byte{192, 168, 1, 1},                     // Target IP address
+	}
+
+	buf := gopacket.NewSerializeBuffer()
+
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+
+	// Serialize the ARP packet.
+	gopacket.SerializeLayers(buf, opts, &eth, &arp)
+
+	return buf.Bytes(), gopacket.CaptureInfo{}, nil
+}
+
+func NewArpRequestReadResult() (data []byte, ci gopacket.CaptureInfo, err error) {
+	eth := layers.Ethernet{
+		SrcMAC:       []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+		DstMAC:       []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+		EthernetType: layers.EthernetTypeARP,
+	}
+
+	arp := layers.ARP{
+		AddrType:          layers.LinkTypeEthernet,
+		Protocol:          layers.EthernetTypeIPv4,
+		HwAddressSize:     6,
+		ProtAddressSize:   4,
+		Operation:         layers.ARPRequest,                          // ARP reply
+		SourceHwAddress:   []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, // Your MAC address
+		SourceProtAddress: []byte{192, 168, 1, 100},                   // Your IP address
+		DstHwAddress:      []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, // Target MAC address
+		DstProtAddress:    []byte{192, 168, 1, 1},                     // Target IP address
+	}
+
+	buf := gopacket.NewSerializeBuffer()
+
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+
+	// Serialize the ARP packet.
+	gopacket.SerializeLayers(buf, opts, &eth, &arp)
+
+	return buf.Bytes(), gopacket.CaptureInfo{}, nil
+}
--- pkg/scanner/arpscan_test.go
+++ pkg/scanner/arpscan_test.go
@@ -10,7 +10,9 @@ import (
 	mock_network "github.com/robgonnella/go-lanscan/mock/network"
 	mock_scanner "github.com/robgonnella/go-lanscan/mock/scanner"
 	mock_vendor "github.com/robgonnella/go-lanscan/mock/vendor"
+	test_helper "github.com/robgonnella/go-lanscan/pkg/internal/test-helper"
 	"github.com/robgonnella/go-lanscan/pkg/scanner"
+	"github.com/robgonnella/go-lanscan/pkg/vendor"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 )
@@ -22,12 +24,14 @@ func TestArpScanner(t *testing.T) {
 
 	mockInterface := &net.Interface{
 		Name:         "interfaceName",
-		HardwareAddr: net.HardwareAddr{},
+		HardwareAddr: net.HardwareAddr([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00}),
 	}
 
 	_, mockIPNet, _ := net.ParseCIDR("172.17.1.1/32")
 
 	mockUserIP := net.ParseIP("172.17.1.1")
+	mockNonIncludedArpSrcIP := net.ParseIP("172.17.1.2")
+	mockIncludedArpSrcIP := net.ParseIP("172.17.1.1")
 
 	t.Run("returns immediately if already scanning", func(st *testing.T) {
 		cap := mock_scanner.NewMockPacketCapture(ctrl)
@@ -50,7 +54,7 @@ func TestArpScanner(t *testing.T) {
 		wg.Add(1)
 
 		mockNetInfo.EXPECT().Interface().AnyTimes().Return(mockInterface)
-		mockNetInfo.EXPECT().IPNet().Return(mockIPNet)
+		mockNetInfo.EXPECT().IPNet().Return(mockIPNet).AnyTimes()
 		mockNetInfo.EXPECT().UserIP().Return(mockUserIP)
 
 		cap.EXPECT().OpenLive(
@@ -72,7 +76,10 @@ func TestArpScanner(t *testing.T) {
 		})
 
 		handle.EXPECT().ReadPacketData().AnyTimes().DoAndReturn(func() (data []byte, ci gopacket.CaptureInfo, err error) {
-			return []byte{}, gopacket.CaptureInfo{}, nil
+			return test_helper.NewArpReplyReadResult(
+				mockNonIncludedArpSrcIP,
+				[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01},
+			)
 		})
 
 		go arpScanner.Scan()
@@ -135,7 +142,7 @@ func TestArpScanner(t *testing.T) {
 		wg.Add(1)
 
 		mockNetInfo.EXPECT().Interface().AnyTimes().Return(mockInterface)
-		mockNetInfo.EXPECT().IPNet().Return(mockIPNet)
+		mockNetInfo.EXPECT().IPNet().Return(mockIPNet).AnyTimes()
 		mockNetInfo.EXPECT().UserIP().Return(mockUserIP)
 
 		cap.EXPECT().OpenLive(
@@ -157,7 +164,10 @@ func TestArpScanner(t *testing.T) {
 		})
 
 		handle.EXPECT().ReadPacketData().AnyTimes().DoAndReturn(func() (data []byte, ci gopacket.CaptureInfo, err error) {
-			return []byte{}, gopacket.CaptureInfo{}, nil
+			return test_helper.NewArpReplyReadResult(
+				mockNonIncludedArpSrcIP,
+				[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01},
+			)
 		})
 
 		err := arpScanner.Scan()
@@ -187,7 +197,7 @@ func TestArpScanner(t *testing.T) {
 		wg.Add(1)
 
 		mockNetInfo.EXPECT().Interface().AnyTimes().Return(mockInterface)
-		mockNetInfo.EXPECT().UserIP().Return(mockUserIP)
+		mockNetInfo.EXPECT().UserIP().Return(mockUserIP).AnyTimes()
 
 		cap.EXPECT().OpenLive(
 			gomock.Any(),
@@ -208,7 +218,10 @@ func TestArpScanner(t *testing.T) {
 		})
 
 		handle.EXPECT().ReadPacketData().AnyTimes().DoAndReturn(func() (data []byte, ci gopacket.CaptureInfo, err error) {
-			return []byte{}, gopacket.CaptureInfo{}, nil
+			return test_helper.NewArpReplyReadResult(
+				mockNonIncludedArpSrcIP,
+				[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01},
+			)
 		})
 
 		err := arpScanner.Scan()
@@ -218,30 +231,449 @@ func TestArpScanner(t *testing.T) {
 		assert.NoError(st, err)
 	})
 
-	// t.Run("handles arp reply", func(st *testing.T) {
-	// 	cap := mock_scanner.NewMockPacketCapture(ctrl)
-	// 	handle := mock_scanner.NewMockPacketCaptureHandle(ctrl)
+	t.Run("ignores non arp reply packets", func(st *testing.T) {
+		cap := mock_scanner.NewMockPacketCapture(ctrl)
+		handle := mock_scanner.NewMockPacketCaptureHandle(ctrl)
+		vendorRepo := mock_vendor.NewMockVendorRepo(ctrl)
+		mockNetInfo := mock_network.NewMockNetwork(ctrl)
 
-	// 	resultChan := make(chan *scanner.ScanResult)
+		resultChan := make(chan *scanner.ScanResult)
 
-	// 	arpScanner := scanner.NewArpScanner(
-	// 		[]string{"172.17.1.1"},
-	// 		netInfo,
-	// 		resultChan,
-	// 		ouiTxt,
-	// 		scanner.WithPacketCapture(cap),
-	// 	)
+		arpScanner := scanner.NewArpScanner(
+			[]string{"172.17.1.1"},
+			mockNetInfo,
+			resultChan,
+			vendorRepo,
+			scanner.WithPacketCapture(cap),
+		)
 
-	// 	cap.EXPECT().OpenLive(
-	// 		gomock.Any(),
-	// 		gomock.Any(),
-	// 		gomock.Any(),
-	// 		gomock.Any()).Return(handle, nil)
+		wg := sync.WaitGroup{}
+		wg.Add(1)
 
-	// 	handle.EXPECT().Close().AnyTimes()
-	// 	handle.EXPECT().ReadPacketData().AnyTimes()
-	// 	handle.EXPECT().WritePacketData(gomock.Any()).AnyTimes()
+		mockNetInfo.EXPECT().Interface().AnyTimes().Return(mockInterface)
+		mockNetInfo.EXPECT().UserIP().Return(mockUserIP).AnyTimes()
 
-	// })
+		cap.EXPECT().OpenLive(
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any()).Return(handle, nil)
 
+		cap.EXPECT().SerializeLayers(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+
+		handle.EXPECT().Close().AnyTimes()
+
+		handle.EXPECT().WritePacketData(gomock.Any()).DoAndReturn(func(data []byte) (err error) {
+			defer func() {
+				arpScanner.Stop()
+				wg.Done()
+			}()
+			return nil
+		})
+
+		handle.EXPECT().ReadPacketData().AnyTimes().DoAndReturn(func() (data []byte, ci gopacket.CaptureInfo, err error) {
+			return test_helper.NewArpRequestReadResult()
+		})
+
+		err := arpScanner.Scan()
+
+		wg.Wait()
+
+		assert.NoError(st, err)
+	})
+
+	t.Run("ignores arp reply packets that originate from scanning host", func(st *testing.T) {
+		cap := mock_scanner.NewMockPacketCapture(ctrl)
+		handle := mock_scanner.NewMockPacketCaptureHandle(ctrl)
+		vendorRepo := mock_vendor.NewMockVendorRepo(ctrl)
+		mockNetInfo := mock_network.NewMockNetwork(ctrl)
+
+		resultChan := make(chan *scanner.ScanResult)
+
+		arpScanner := scanner.NewArpScanner(
+			[]string{"172.17.1.1"},
+			mockNetInfo,
+			resultChan,
+			vendorRepo,
+			scanner.WithPacketCapture(cap),
+		)
+
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+
+		mockNetInfo.EXPECT().Interface().AnyTimes().Return(mockInterface)
+		mockNetInfo.EXPECT().UserIP().Return(mockUserIP).AnyTimes()
+
+		cap.EXPECT().OpenLive(
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any()).Return(handle, nil)
+
+		cap.EXPECT().SerializeLayers(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+
+		handle.EXPECT().Close().AnyTimes()
+
+		handle.EXPECT().WritePacketData(gomock.Any()).DoAndReturn(func(data []byte) (err error) {
+			defer func() {
+				arpScanner.Stop()
+				wg.Done()
+			}()
+			return nil
+		})
+
+		handle.EXPECT().ReadPacketData().AnyTimes().DoAndReturn(func() (data []byte, ci gopacket.CaptureInfo, err error) {
+			return test_helper.NewArpReplyReadResult(
+				mockNonIncludedArpSrcIP,
+				[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+			)
+		})
+
+		err := arpScanner.Scan()
+
+		wg.Wait()
+
+		assert.NoError(st, err)
+	})
+
+	t.Run("process valid arp reply packet", func(st *testing.T) {
+		cap := mock_scanner.NewMockPacketCapture(ctrl)
+		handle := mock_scanner.NewMockPacketCaptureHandle(ctrl)
+		vendorRepo := mock_vendor.NewMockVendorRepo(ctrl)
+		mockNetInfo := mock_network.NewMockNetwork(ctrl)
+
+		resultChan := make(chan *scanner.ScanResult)
+
+		arpScanner := scanner.NewArpScanner(
+			[]string{"172.17.1.1"},
+			mockNetInfo,
+			resultChan,
+			vendorRepo,
+			scanner.WithPacketCapture(cap),
+		)
+
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+
+		mockNetInfo.EXPECT().Interface().AnyTimes().Return(mockInterface)
+		mockNetInfo.EXPECT().UserIP().Return(mockUserIP).AnyTimes()
+
+		cap.EXPECT().OpenLive(
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any()).Return(handle, nil)
+
+		cap.EXPECT().SerializeLayers(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+
+		handle.EXPECT().Close().AnyTimes()
+
+		handle.EXPECT().WritePacketData(gomock.Any()).DoAndReturn(func(data []byte) (err error) {
+			defer func() {
+				arpScanner.Stop()
+				wg.Done()
+			}()
+			return nil
+		})
+
+		handle.EXPECT().ReadPacketData().AnyTimes().DoAndReturn(func() (data []byte, ci gopacket.CaptureInfo, err error) {
+			return test_helper.NewArpReplyReadResult(
+				mockIncludedArpSrcIP,
+				[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01},
+			)
+		})
+
+		err := arpScanner.Scan()
+
+		wg.Wait()
+
+		assert.NoError(st, err)
+	})
+
+	t.Run("process valid arp reply packet and includes vendor info", func(st *testing.T) {
+		cap := mock_scanner.NewMockPacketCapture(ctrl)
+		handle := mock_scanner.NewMockPacketCaptureHandle(ctrl)
+		vendorRepo := mock_vendor.NewMockVendorRepo(ctrl)
+		mockNetInfo := mock_network.NewMockNetwork(ctrl)
+
+		resultChan := make(chan *scanner.ScanResult)
+
+		arpScanner := scanner.NewArpScanner(
+			[]string{"172.17.1.1"},
+			mockNetInfo,
+			resultChan,
+			vendorRepo,
+			scanner.WithPacketCapture(cap),
+			scanner.WithVendorInfo(true),
+		)
+
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+
+		mockNetInfo.EXPECT().Interface().AnyTimes().Return(mockInterface)
+		mockNetInfo.EXPECT().UserIP().Return(mockUserIP).AnyTimes()
+
+		cap.EXPECT().OpenLive(
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any()).Return(handle, nil)
+
+		cap.EXPECT().SerializeLayers(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+
+		handle.EXPECT().Close().AnyTimes()
+
+		handle.EXPECT().WritePacketData(gomock.Any()).DoAndReturn(func(data []byte) (err error) {
+			defer func() {
+				arpScanner.Stop()
+				wg.Done()
+			}()
+			return nil
+		})
+
+		handle.EXPECT().ReadPacketData().AnyTimes().DoAndReturn(func() (data []byte, ci gopacket.CaptureInfo, err error) {
+			return test_helper.NewArpReplyReadResult(
+				mockIncludedArpSrcIP,
+				[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01},
+			)
+		})
+
+		vendorRepo.EXPECT().Query(gomock.Any()).AnyTimes().Return(
+			&vendor.VendorResult{
+				Name: "Apple",
+			},
+			nil,
+		)
+
+		err := arpScanner.Scan()
+
+		wg.Wait()
+
+		assert.NoError(st, err)
+	})
+
+	t.Run("handles vendor query error", func(st *testing.T) {
+		cap := mock_scanner.NewMockPacketCapture(ctrl)
+		handle := mock_scanner.NewMockPacketCaptureHandle(ctrl)
+		vendorRepo := mock_vendor.NewMockVendorRepo(ctrl)
+		mockNetInfo := mock_network.NewMockNetwork(ctrl)
+
+		resultChan := make(chan *scanner.ScanResult)
+
+		arpScanner := scanner.NewArpScanner(
+			[]string{"172.17.1.1"},
+			mockNetInfo,
+			resultChan,
+			vendorRepo,
+			scanner.WithPacketCapture(cap),
+			scanner.WithVendorInfo(true),
+		)
+
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+
+		mockNetInfo.EXPECT().Interface().AnyTimes().Return(mockInterface)
+		mockNetInfo.EXPECT().UserIP().Return(mockUserIP).AnyTimes()
+
+		cap.EXPECT().OpenLive(
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any()).Return(handle, nil)
+
+		cap.EXPECT().SerializeLayers(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+
+		handle.EXPECT().Close().AnyTimes()
+
+		handle.EXPECT().WritePacketData(gomock.Any()).DoAndReturn(func(data []byte) (err error) {
+			defer func() {
+				arpScanner.Stop()
+				wg.Done()
+			}()
+			return nil
+		})
+
+		handle.EXPECT().ReadPacketData().AnyTimes().DoAndReturn(func() (data []byte, ci gopacket.CaptureInfo, err error) {
+			return test_helper.NewArpReplyReadResult(
+				mockIncludedArpSrcIP,
+				[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01},
+			)
+		})
+
+		vendorRepo.EXPECT().Query(gomock.Any()).AnyTimes().Return(
+			nil,
+			errors.New("mock vendor query error"),
+		)
+
+		err := arpScanner.Scan()
+
+		wg.Wait()
+
+		assert.NoError(st, err)
+	})
+
+	t.Run("calls request notification callback", func(st *testing.T) {
+		cap := mock_scanner.NewMockPacketCapture(ctrl)
+		handle := mock_scanner.NewMockPacketCaptureHandle(ctrl)
+		vendorRepo := mock_vendor.NewMockVendorRepo(ctrl)
+		mockNetInfo := mock_network.NewMockNetwork(ctrl)
+
+		callback := func(request *scanner.Request) {
+			assert.NotNil(st, request)
+		}
+
+		resultChan := make(chan *scanner.ScanResult)
+
+		arpScanner := scanner.NewArpScanner(
+			[]string{"172.17.1.1"},
+			mockNetInfo,
+			resultChan,
+			vendorRepo,
+			scanner.WithPacketCapture(cap),
+			scanner.WithRequestNotifications(callback),
+		)
+
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+
+		mockNetInfo.EXPECT().Interface().AnyTimes().Return(mockInterface)
+		mockNetInfo.EXPECT().UserIP().Return(mockUserIP).AnyTimes()
+
+		cap.EXPECT().OpenLive(
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any()).Return(handle, nil)
+
+		cap.EXPECT().SerializeLayers(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+
+		handle.EXPECT().Close().AnyTimes()
+
+		handle.EXPECT().WritePacketData(gomock.Any()).DoAndReturn(func(data []byte) (err error) {
+			defer func() {
+				arpScanner.Stop()
+				wg.Done()
+			}()
+			return nil
+		})
+
+		handle.EXPECT().ReadPacketData().AnyTimes().DoAndReturn(func() (data []byte, ci gopacket.CaptureInfo, err error) {
+			return test_helper.NewArpReplyReadResult(
+				mockIncludedArpSrcIP,
+				[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01},
+			)
+		})
+
+		vendorRepo.EXPECT().Query(gomock.Any()).AnyTimes().Return(
+			nil,
+			errors.New("mock vendor query error"),
+		)
+
+		err := arpScanner.Scan()
+
+		wg.Wait()
+
+		assert.NoError(st, err)
+	})
+
+	t.Run("handles serialize layers error", func(st *testing.T) {
+		cap := mock_scanner.NewMockPacketCapture(ctrl)
+		handle := mock_scanner.NewMockPacketCaptureHandle(ctrl)
+		vendorRepo := mock_vendor.NewMockVendorRepo(ctrl)
+		mockNetInfo := mock_network.NewMockNetwork(ctrl)
+
+		callback := func(request *scanner.Request) {
+			assert.NotNil(st, request)
+		}
+
+		resultChan := make(chan *scanner.ScanResult)
+
+		arpScanner := scanner.NewArpScanner(
+			[]string{"172.17.1.1"},
+			mockNetInfo,
+			resultChan,
+			vendorRepo,
+			scanner.WithPacketCapture(cap),
+			scanner.WithRequestNotifications(callback),
+		)
+
+		mockNetInfo.EXPECT().Interface().AnyTimes().Return(mockInterface)
+		mockNetInfo.EXPECT().UserIP().Return(mockUserIP).AnyTimes()
+
+		cap.EXPECT().OpenLive(
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any()).Return(handle, nil)
+
+		cap.EXPECT().SerializeLayers(
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any()).
+			AnyTimes().
+			Return(errors.New("mock serialize layers error"))
+
+		handle.EXPECT().Close().AnyTimes()
+
+		handle.EXPECT().ReadPacketData().AnyTimes().DoAndReturn(func() (data []byte, ci gopacket.CaptureInfo, err error) {
+			return test_helper.NewArpReplyReadResult(
+				mockIncludedArpSrcIP,
+				[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01},
+			)
+		})
+
+		err := arpScanner.Scan()
+
+		assert.Error(st, err)
+	})
+
+	t.Run("handles write packet data error", func(st *testing.T) {
+		cap := mock_scanner.NewMockPacketCapture(ctrl)
+		handle := mock_scanner.NewMockPacketCaptureHandle(ctrl)
+		vendorRepo := mock_vendor.NewMockVendorRepo(ctrl)
+		mockNetInfo := mock_network.NewMockNetwork(ctrl)
+
+		callback := func(request *scanner.Request) {
+			assert.NotNil(st, request)
+		}
+
+		resultChan := make(chan *scanner.ScanResult)
+
+		arpScanner := scanner.NewArpScanner(
+			[]string{"172.17.1.1"},
+			mockNetInfo,
+			resultChan,
+			vendorRepo,
+			scanner.WithPacketCapture(cap),
+			scanner.WithRequestNotifications(callback),
+		)
+
+		mockNetInfo.EXPECT().Interface().AnyTimes().Return(mockInterface)
+		mockNetInfo.EXPECT().UserIP().Return(mockUserIP).AnyTimes()
+
+		cap.EXPECT().OpenLive(
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any()).Return(handle, nil)
+
+		cap.EXPECT().SerializeLayers(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+
+		handle.EXPECT().WritePacketData(gomock.Any()).DoAndReturn(func(data []byte) (err error) {
+			return errors.New("mock write packet data error")
+		})
+
+		handle.EXPECT().Close().AnyTimes()
+
+		handle.EXPECT().ReadPacketData().AnyTimes().DoAndReturn(func() (data []byte, ci gopacket.CaptureInfo, err error) {
+			return test_helper.NewArpReplyReadResult(
+				mockIncludedArpSrcIP,
+				[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01},
+			)
+		})
+
+		err := arpScanner.Scan()
+
+		assert.Error(st, err)
+	})
 }
